7070e1a26786693f4622a8b6c6256f1e4ff2fdb2
morningdew72 <jacob.merwede@gmail.com>
2020-07-19 11:33:08 -0400

    Tk file explorer, while loop to re-run program

--- vantage_combine_csv.py
+++ vantage_combine_csv.py
@@ -1,63 +1,65 @@
 import pandas as pd
-import os
+from tkinter.filedialog import askopenfilename
+import tkinter as tk
 
-start = input("Are both .csv files present? Type 'yes' and hit Enter.  ")
-if start != 'yes':
-	exit()
-else:
-	pass
+def csv_combine():
+	print("Select the Qualitative CSV File")
+	proceed = input("Hit Enter to Continue")
+	tk.Tk().withdraw()
+	csv_qual = askopenfilename()
+	print(csv_qual)
 
-run_name = input("Enter a name for the run (no spaces or / ): ")
-output_file_name = run_name + '_combined_file.csv'
+	print("Select the RUO CSV File")
+	proceed = input("Hit Enter to Continue")
+	csv_ruo = askopenfilename()
+	print(csv_ruo)
 
-csvs = []
-files = os.listdir()
-for file in files:
-    if file[-3:] == "csv":
-        csvs.append(file)
-    else:
-        pass
-for csv in csvs:
-    if "_RUO" in csv:
-        csv_ruo = csv
-    elif "sample_results" in csv:
-        csv_qual = csv
-    else:
-        pass
-    
-df_qual = pd.read_csv(csv_qual, header=11)
-df_ruo = pd.read_csv(csv_ruo)
+	run_name = input("Enter a name for the run (no spaces or / ): ")
+	output_file_name = run_name + '_combined_file.csv'
+	  
+	df_qual = pd.read_csv(csv_qual, skiprows=12)
+	df_qual.to_csv('testqual.csv', index=False)
+	df_ruo = pd.read_csv(csv_ruo)
 
-print("RUO File: ", csv_ruo)
-print("Qual File: ", csv_qual)
+	print("RUO File: ", csv_ruo)
+	print("Qual File: ", csv_qual)
 
-def well_standard(x):
-    if len(x) < 3:
-        alpha = x[:1]
-        numb = x[1:]
-        numb = '0' + numb
-        x = alpha + numb
-        return(x)
-    else:
-        return(x)
+	def well_standard(x):
+	    if len(x) < 3:
+	        alpha = x[:1]
+	        numb = x[1:]
+	        numb = '0' + numb
+	        x = alpha + numb
+	        return(x)
+	    else:
+	        return(x)
 
-df_ruo['Well'] = df_ruo['Well'].apply(well_standard)
+	df_ruo['Well'] = df_ruo['Well'].apply(well_standard)
 
-df_new = df_ruo
-df_new['Interpretive Result'] = None
-df_new['Action*'] = None
-df_new = df_new.drop(df_new.columns[[5, 10, 11, 12, 13, 14]], axis=1)
-well_list = list(set(df_new['Well'].to_list()))
+	df_new = df_ruo
+	df_new['Interpretive Result'] = None
+	df_new['Action*'] = None
+	df_new = df_new.drop(df_new.columns[[5, 10, 11, 12, 13, 14]], axis=1)
+	well_list = list(set(df_new['Well'].to_list()))
 
-for w in well_list:
-    index_list = df_qual.index[df_qual['Well'] == w]
-    index = int(index_list[0])
-    sample = df_qual.iloc[index, 2]
-    inter_result = df_qual.iloc[index, 6]
-    action = df_qual.iloc[index, 7]
-    new_row = {'Well':w, 'Sample':sample, 'Target':'A_Qual', 'Interpretive Result':inter_result, 'Action*':action}
-    df_new = df_new.append(new_row, ignore_index=True)
+	for w in well_list:
+	    index_list = df_qual.index[df_qual['Well'] == w]
+	    index = int(index_list[0])
+	    sample = df_qual.iloc[index, 2]
+	    inter_result = df_qual.iloc[index, 6]
+	    action = df_qual.iloc[index, 7]
+	    new_row = {'Well':w, 'Sample':sample, 'Target':'A_Qual', 'Interpretive Result':inter_result, 'Action*':action}
+	    df_new = df_new.append(new_row, ignore_index=True)
 
-df_new = df_new.sort_values(by = ['Well', 'Target'])
+	df_new = df_new.sort_values(by = ['Well', 'Target'])
 
-df_new.to_csv(output_file_name, index=False)
+	df_new.to_csv(output_file_name, index=False)
+
+csv_combine()
+start_again = 'yes'
+while start_again == 'yes':
+	start_again = input("Do you want to run the program again? (Type 'yes' to continue):  ")
+	if start_again == 'yes':
+		csv_combine()
+	else:
+		start_again = 'no'
