d6a4f2aea1f9893a26ed5cd4b0379cf4265b2aab
morningdew72 <jacob.merwede@gmail.com>
2020-07-19 11:26:13 -0400

    Created '5th Target' line for Qual data, removed columns

--- vantage_combine_csv.py
+++ vantage_combine_csv.py
@@ -18,15 +18,18 @@ for file in files:
     else:
         pass
 for csv in csvs:
-    if "RUO" in csv:
+    if "_RUO" in csv:
         csv_ruo = csv
-    elif "Qual" in csv:
+    elif "sample_results" in csv:
         csv_qual = csv
     else:
         pass
     
-df_qual = pd.read_csv(csv_qual, header=12)
+df_qual = pd.read_csv(csv_qual, header=11)
 df_ruo = pd.read_csv(csv_ruo)
+
+print("RUO File: ", csv_ruo)
+print("Qual File: ", csv_qual)
 
 def well_standard(x):
     if len(x) < 3:
@@ -39,5 +42,22 @@ def well_standard(x):
         return(x)
 
 df_ruo['Well'] = df_ruo['Well'].apply(well_standard)
-df_join = pd.merge(df_ruo, df_qual, on='Well', how='right')
-df_join.to_csv(output_file_name)
+
+df_new = df_ruo
+df_new['Interpretive Result'] = None
+df_new['Action*'] = None
+df_new = df_new.drop(df_new.columns[[5, 10, 11, 12, 13, 14]], axis=1)
+well_list = list(set(df_new['Well'].to_list()))
+
+for w in well_list:
+    index_list = df_qual.index[df_qual['Well'] == w]
+    index = int(index_list[0])
+    sample = df_qual.iloc[index, 2]
+    inter_result = df_qual.iloc[index, 6]
+    action = df_qual.iloc[index, 7]
+    new_row = {'Well':w, 'Sample':sample, 'Target':'A_Qual', 'Interpretive Result':inter_result, 'Action*':action}
+    df_new = df_new.append(new_row, ignore_index=True)
+
+df_new = df_new.sort_values(by = ['Well', 'Target'])
+
+df_new.to_csv(output_file_name, index=False)
